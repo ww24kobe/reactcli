de71785946f6b74ee01821195bafb9eefa6d62b7
wangwei <wangwei@163.com>
2021-06-22 10:19:21 +0800

    'feat:login'

--- src/components/Bcom/index.jsx
+++ src/components/Bcom/index.jsx
@@ -1,13 +1,13 @@
 import React,{Component} from 'react'
-
+import './index.scss';
 export default class B extends Component {
   constructor(props){
     super(props)
   }
   render() {
     return (
-      <div>
-        B组件 - {this.props.title}
+      <div class="bwrap">
+        <h2>B组件 - {this.props.title}</h2>
       </div>
     );
   }
